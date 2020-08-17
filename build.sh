a7466d0afd2cb6f38e8593531afa163b0e9528ae
s00567839 <s00567839@notesmail.huawei.com>
2020-08-17 16:08:09 +0800

    TicketNo:AR000E1RKT Description:modify gcc's path Team:OTHERS Feature or Bugfix:Bugfix Binary Source:No PrivateCode(Yes/No):No
    Change-Id: Ie2ac8089d3a14bdc6152da7eb7fb02d45b2f413e
    Reviewed-on: http://mgit-tm.rnd.huawei.com/10276107
    Tested-by: public jenkins <public_jenkins@notesmail.huawei.com>
    Reviewed-by: zhangyukun 00562790 <zhangyukun8@huawei.com>
    Reviewed-by: zhangguyuan 00314551 <zhangguyuan@huawei.com>

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ ROOT_DIR=$(dirname "$0")
 git checkout -- configure
 git apply huawei_libjpeg_patch1.patch
 
-./configure CC=$ROOT_DIR/../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/bin/arm-linux-ohoseabi-gcc --host=arm-linux
+./configure CC=$ROOT_DIR/../../prebuilts/gcc/linux-x86/arm/arm-linux-ohoseabi-gcc/bin/arm-linux-ohoseabi-gcc --host=arm-linux
 
 make clean
 make -j
-$ROOT_DIR/../../prebuilts/gcc/linux-x86/arm/arm-linux-harmonyeabi-gcc/arm-linux-ohoseabi/bin/strip $ROOT_DIR/.libs/libjpeg.so
+$ROOT_DIR/../../prebuilts/gcc/linux-x86/arm/arm-linux-ohoseabi-gcc/arm-linux-ohoseabi/bin/strip $ROOT_DIR/.libs/libjpeg.so
 cp $ROOT_DIR/.libs/libjpeg.so $OUT_DIR/libjpeg.so
